58f3429374f22bd04f85d8219349f0188fb01aa2
JustLuaDeveloper <stasmaks5423@gmail.com>
2026-02-23 11:57:59 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local Games = {
   ['9338511689'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/NFTbattle.lua',
   ['7935634976'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/ThelostFront.lua',
   ['4795326392'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/PullASword.lua',
-  ['8520148363'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/TankGame.lua'
+  ['8520148363'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/TankGame.lua',
+  ['8191429227'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/CutTrees.lua'
 }
 if Games[tostring(game.GameId)] then loadstring(game:HttpGet(Games[tostring(game.GameId)]))() end
