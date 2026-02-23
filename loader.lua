6e2350a237b16a2b8bb646a4d1ac366bceba9f14
JustLuaDeveloper <stasmaks5423@gmail.com>
2026-02-23 11:07:52 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
 local Games = {
-  ['9338511689'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/NFTbattle.lua'
+  ['9338511689'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/NFTbattle.lua',
+  ['7935634976'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/ThelostFront.lua'
 }
 if Games[tostring(game.GameId)] then loadstring(game:HttpGet(Games[tostring(game.GameId)]))() end
