4ac3ada6852af5f271a2baadef32cf90714190e2
JustLuaDeveloper <stasmaks5423@gmail.com>
2026-02-23 10:47:46 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,4 @@
-
+local Games = {
+  ['9338511689'] = 'https://raw.githubusercontent.com/JustLuaDeveloper/WhyHubMain/refs/heads/main/Scripts/NFTbattle.lua'
+}
+if Games[tostring(game.GameId)] then loadstring(game:HttpGet(Games[tostring(game.GameId)]))() end
